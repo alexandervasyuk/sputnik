2ebaa3fd86f90add0c76f99a0db24c88d4edc29c
Alexander Vasyuk <alexandervasyuk@hotmail.com>
2012-11-12 18:23:16 -0800

    Signup/timezone fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       sign_in(@user, params[:timezone])
-      flash[:success] = "Welcome to the Sputnik!"
+      flash[:success] = "Welcome to the Happpening!"
       redirect_to root_path
     else
       render 'static_pages/home'
