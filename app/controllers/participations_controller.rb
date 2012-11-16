73376281a07d177ca5729b2dcaabbfd2642a3c74
Alexander Vasyuk <alexandervasyuk@hotmail.com>
2012-11-15 20:43:28 -0800

    Fixing participate button took out ajax

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -1,22 +1,24 @@
 class ParticipationsController < ApplicationController
 	before_filter :signed_in_user
-	respond_to :html, :js
+	# respond_to :html, :js
 
 	def create
 		@micropost = Micropost.find(params[:participation][:micropost_id])
 		current_user.participate!(@micropost)
 		
 		#Send the email out
-		MicropostMailer.participated(@micropost.user, @micropost).deliver
-		
-		@user_and_post = [current_user, @micropost]
-		respond_with @user_and_post
+		#MicropostMailer.participated(@micropost.user, @micropost).deliver
+
+		redirect_to :back
+		# @user_and_post = [current_user, @micropost]
+		# respond_with @user_and_post
 	end
 
 	def destroy
 		@micropost = Participation.find(params[:id]).micropost
 		current_user.unparticipate!(@micropost)
-		@user_and_post = [current_user, @micropost]
-		respond_with @user_and_post	
+		redirect_to :back
+		# @user_and_post = [current_user, @micropost]
+		# respond_with @user_and_post	
 	end
 end
