a4a410b998c2ce3908a4eea1fb81a48b67dd35ca
Bo Chen <bochen303@gmail.com>
2013-02-09 19:55:14 -0800

    added tests for posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,31 +1,34 @@
 class Post < ActiveRecord::Base
-  after_save :update_micropost
-  before_destroy :update_micropost
+	attr_accessible :content, :micropost_id, :user_id, :photo
 
-  attr_accessible :content, :micropost_id, :user_id, :photo
+	has_attached_file :photo, styles: { standard: "400x400>" }, 
+		:path => ":rails_root/public/assets/posts/:id/:style/:basename.:extension",
+		:storage => :s3,
+		:s3_credentials => "#{Rails.root}/config/s3.yml"
 
-  has_attached_file :photo, styles: { standard: "400x400>" }, 
-     :path => ":rails_root/public/assets/posts/:id/:style/:basename.:extension",
-     :storage => :s3,
-     :s3_credentials => "#{Rails.root}/config/s3.yml"
-  
-  belongs_to :user
-  belongs_to :micropost
+	# AR Associations
+	belongs_to :user
+	belongs_to :micropost
 
-  validates :content, presence: true
-  validates :user_id, presence: true
-  validates :micropost_id, presence: true
+	# Validations
+	validates :content, presence: true
+	validates :user_id, presence: true
+	validates :micropost_id, presence: true
+
+	def to_mobile
+		replier = self.user
+
+		{replier_picture: replier.avatar.url, reply_text: self.content, replier_name: replier.name, posted_time: self.created_at, posted_id: self.id}
+	end
+
+	def update_micropost
+		micropost = self.micropost
+
+		micropost.updated_at = Time.now
+		micropost.save
+	end
   
-  def to_mobile
-	replier = self.user
-		
-	return {replier_picture: replier.avatar.url, reply_text: self.content, replier_name: replier.name, posted_time: self.created_at, posted_id: self.id}
-  end
-  
-  def update_micropost
-	micropost = self.micropost
-	
-	micropost.updated_at = Time.now
-	micropost.save
-  end
+	# AR Callbacks
+	after_save :update_micropost
+	before_destroy :update_micropost
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,53 @@
 require 'spec_helper'
 
 describe Post do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+	@user = FactoryGirl.create(:user)
+	@micropost = FactoryGirl.create(:micropost, user: @user)
+	
+	@post = FactoryGirl.create(:post, user: @user, micropost: @micropost)
+  end
+  
+  subject { @post }
+  
+  it { should respond_to(:user) }
+  it { should respond_to(:micropost) }
+  
+  # Validation Testing
+  describe "creating a new post" do
+	it "should not create a post without content or photo" do
+		post = Post.new(micropost_id: @micropost.id, user_id: @user.id)
+		
+		post.should_not be_valid
+	end
+	
+	it "should not create a post without a user" do
+		post = Post.new(content: "asdfasdf", micropost_id: @micropost.id)
+		
+		post.should_not be_valid
+	end
+	
+	it "should not create a post without a micropost" do
+		post = Post.new(content: "asdfasdf", user_id: @user.id)
+		
+		post.should_not be_valid
+	end
+  end
+  
+  # AR Callback Testing
+  describe "creating a new post" do
+	it "should update the micropost updated at field" do
+		expect do
+			new_post = FactoryGirl.create(:post, micropost: @micropost, user: @user)
+		end.to change { @micropost.reload.updated_at }
+	end
+  end
+  
+  describe "deleting a post" do
+	it "should update the micropost updated at field" do
+		expect do
+			@post.destroy
+		end.to change { @micropost.reload.updated_at }
+	end
+  end
 end
